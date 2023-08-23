dd4b9e4865278c439ece4d55b7e119a3644e8a97
1448730 <90043456+1448730@users.noreply.github.com>
2023-08-23 16:16:48 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,17 @@ import { RouterModule } from '@angular/router';
     HomeComponent,
     RouterModule,
   ],
-  template: `<main>
-  <a [routerLink]="['/']">
+  template: `
+  <main>
+    <a [routerLink]="['/']">
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
-  </a>
-  <section class="content">
+    </a>
+    <section class="content">
     <router-outlet></router-outlet>
-  </section>
-</main>`,
+    </section>
+  </main>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
